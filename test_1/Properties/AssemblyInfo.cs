30746fdbdbc9873be9231398696c8e8963a5bb2d
treveld <treveld@WNBKNOR023>
2023-03-28 14:23:57 +0200

    Add project files.

---
+++ test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ACCF3973-73F9-43E6-9D20-1D73A408EA03")]
+[assembly: AssemblyVersion("1.0.0.0")]
